9c3d2e292dac9e898fc74bb495ddc7ee4d019ba8
kimizuka <f.kimizuka@gmail.com>
2024-05-16 18:27:06 +0900

    blink

---
+++ index.js
@@ -0,0 +1,42 @@
+const DMX = require('dmx');
+const { app, BrowserWindow } = require('electron/main');
+
+const path = '/dev/tty.usbserial-EN437503';
+const dmx = new DMX();
+const universe = dmx.addUniverse('demo', 'enttec-usb-dmx-pro', path);
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  let isOn = false;
+
+  setInterval(() => {
+    const power = Number(isOn) * 100;
+
+    universe.update({ 2: power });
+    console.log(power);
+
+    isOn = !isOn;
+  }, 1000);
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
